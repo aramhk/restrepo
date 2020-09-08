f459d1bb8af4436a6c453d2f7b25344381241c16
aramhk <48241209+aramhk@users.noreply.github.com>
2020-09-08 11:36:47 +0200

    Add files via upload

---
+++ sim.py
@@ -0,0 +1,29 @@
+import requests
+import xml.etree.ElementTree as ET
+
+
+URL = 'https://xml2.travelsim.com/tsim_xml/service/xmlgate'
+
+phone = input("Please enter Phone number:")
+phone = int(phone)
+
+
+PARAMS =  {'uname':'QpT1ErTYqv', 'upass':'PeXOuiiBph','plain':1, 'command':'account' }
+r= requests.get(url=URL,params=PARAMS)
+print(r.text)
+root = ET.fromstring(r.text)
+oid=root[5].text
+orderid= int(oid) + 1
+
+
+PARAMS1 = {'uname':'QpT1ErTYqv', 'upass':'PeXOuiiBph','plain':1, 'command':'corp','onum':phone ,'corp_minlimit':1,'corp_transaction':4,'corp_maxlimit':50,'aserviceid':24181,'corp_group':1,'corp_enabled':'yes' } 
+PARAMS2 = {'uname':'QpT1ErTYqv', 'upass':'PeXOuiiBph','plain':1, 'command':'sbalance','amount': '+1.00' ,'curr':'EUR','orderid':orderid,'onum':phone ,'groupid':1,'aserviceid':24181 }
+
+
+
+print('----------------------------------------------------------------------------')
+r1 = requests.get(url=URL,params=PARAMS1)
+print(r1.text)
+print('----------------------------------------------------------------------------')
+r2 = requests.get(url=URL,params=PARAMS2)
+print(r2.text)
